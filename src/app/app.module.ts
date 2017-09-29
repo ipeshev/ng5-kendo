e707ee506fd6831fcde8a84511846674a33bb258
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-09-29 15:23:05 +0300

    chore: workaround for compilation error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { InputsModule } from '@progress/kendo-angular-inputs';
 
+/**
+ * The ResizeSensorModule must be imported with 5.0.0-rc.0, otherwise
+ * the compilation fails with:
+ *
+ *  ERROR in /home/tsonev/tmp/ng5-test1/src/$$_gendir/node_modules/@progress/kendo-angular-inputs/dist/es/slider/slider.component.ngfactory.ts (19,22): Cannot find module '../../../../kendo-angular-resize-sensor/dist/es/resize-sensor.component.ngfactory'.
+ *  ERROR in /home/tsonev/tmp/ng5-test1/src/$$_gendir/node_modules/@progress/kendo-angular-inputs/dist/es/switch/switch.component.ngfactory.ts (15,21): Cannot find module '../../../../kendo-angular-resize-sensor/dist/es/resize-sensor.component.ngfactory'.
+ *
+ */
+import { ResizeSensorModule } from '@progress/kendo-angular-resize-sensor';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
